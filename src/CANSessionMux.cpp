7f11a98738de5d670959a78b7dc5f4eb68ab9429
h <dangerousElectrician@gmail.com>
2015-11-18 13:27:49 -0600

    stuff

---
+++ src/CANSessionMux.cpp
@@ -0,0 +1,30 @@
+
+#include "CANSessionMux.h"
+
+#include <iostream>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif  
+        
+	void FRC_NetworkCommunication_CANSessionMux_sendMessage(uint32_t messageID, const uint8_t *data, uint8_t dataSize, int32_t periodMs, int32_t *status) {
+		std::cout << "sendCAN" << std::endl;
+	}
+	void FRC_NetworkCommunication_CANSessionMux_receiveMessage(uint32_t *messageID, uint32_t messageIDMask, uint8_t *data, uint8_t *dataSize, uint32_t *timeStamp, int32_t *status) {
+		std::cout << "recvCAN" << std::endl;
+	}
+	void FRC_NetworkCommunication_CANSessionMux_openStreamSession(uint32_t *sessionHandle, uint32_t messageID, uint32_t messageIDMask, uint32_t maxMessages, int32_t *status) {
+		std::cout << "openStreamCAN" << std::endl;
+	}
+	void FRC_NetworkCommunication_CANSessionMux_closeStreamSession(uint32_t sessionHandle) {
+		std::cout << "closeStreamCAN" << std::endl;
+	}
+	void FRC_NetworkCommunication_CANSessionMux_readStreamSession(uint32_t sessionHandle, struct tCANStreamMessage *messages, uint32_t messagesToRead, uint32_t *messagesRead, int32_t *status) {
+		std::cout << "readStreamCAN" << std::endl;
+	}
+	//void FRC_NetworkCommunication_CANSessionMux_getCANStatus(float *percentBusUtilization, uint32_t *busOffCount, uint32_t *txFullCount, uint32_t *receiveErrorCount, uint32_t *transmitErrorCount, int32_t *status);
+
+#ifdef __cplusplus
+}
+#endif
