33755b32ffff1b81a9f3984bc3ce65a310b8f510
h <dangerousElectrician@gmail.com>
2015-11-18 18:32:34 -0600

    how 2 compile

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,7 @@
 #include "CanTalonSRX.h"
-#include <memory>
 #include <iostream>
 
 int main() {
 	CanTalonSRX motor (0);
-	std::cout << "hello";
-	//std::unique_ptr<CanTalonSRX> m_impl;
-	//m_impl(new CanTalonSRX(0));
+	std::cout << "hello" << std::endl;
 }
