a6af015bcb462f185cb7a721141e31567bc05c1b
DangerousElectrician <DangerousElectrician@gmail.com>
2015-12-24 11:47:51 -0600

    added a message

--- src/can_talon_ros/src/test.cpp
+++ src/can_talon_ros/src/test.cpp
@@ -2,23 +2,35 @@
 #include <iostream>
 #include "ros/ros.h"
 
+#include "can_talon_ros/CANSend.h"
+//#include "std_msgs/String.h"
+
 //#include "std_msgs/String.h"
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "talontest");
-
-	CanTalonSRX motor (0);
+	ros::NodeHandle n;
 	
-	//std::cout << "setprofile" << std::endl;
-	//motor.SetProfileSlotSelect(1);	
-	std::cout << "setmode" << std::endl;
-	motor.SetModeSelect(4, 133);
+	ros::Publisher CANSend_pub = n.advertise<can_talon_ros::CANSend>("CANSend",100);
+	//ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
-	std::cout << "set1" << std::endl;
-	motor.Set(.1);
+	ros::Rate loop_rate(100);
 
-	std::cout << "set2" << std::endl;
-	motor.Set(1);
+	while(ros::ok()) {
 
-	std::cout << "done" << std::endl;
+		CanTalonSRX motor (0);
+		
+		//std::cout << "setprofile" << std::endl;
+		//motor.SetProfileSlotSelect(1);	
+		std::cout << "setmode" << std::endl;
+		motor.SetModeSelect(4, 133);
+
+		std::cout << "set1" << std::endl;
+		motor.Set(.1);
+
+		std::cout << "set2" << std::endl;
+		motor.Set(1);
+
+		std::cout << "done" << std::endl;
+	}
 }
