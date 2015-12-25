2981a32231a9f754fe0c5608a31fc3de81ed9bf2
DangerousElectrician <DangerousElectrician@gmail.com>
2015-12-24 19:40:51 -0600

    publish msg

--- src/can_talon_ros/src/test.cpp
+++ src/can_talon_ros/src/test.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
 	
 	ros::Publisher CANSend_pub = n.advertise<can_talon_ros::CANSend>("CANSend",100);
 
-	ros::Rate loop_rate(500);
+	ros::Rate loop_rate(2); //in hertz
 
 	while(ros::ok()) {
 		
@@ -23,7 +23,11 @@ int main(int argc, char **argv) {
 		msg.arbID = 32;
 		msg.size = 255;
 		std::vector<uint8_t> candata = {1,2,3,4,5,6,7,8};
+		//std::cout<<candata.size()<<std::endl;
 		msg.data = candata;
+		
+		CANSend_pub.publish(msg);
+
 		//CanTalonSRX motor (0);
 		
 		//std::cout << "setprofile" << std::endl;
