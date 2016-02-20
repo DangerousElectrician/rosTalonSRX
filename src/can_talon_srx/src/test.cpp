f401d237cf7bffd107186185deaf551a20419ff5
h <dangerousElectrician@gmail.com>
2016-02-19 21:01:20 -0600

    cleaned it up a little

--- src/can_talon_srx/src/test.cpp
+++ src/can_talon_srx/src/test.cpp
@@ -5,12 +5,10 @@
 #include "FRC_NetworkCommunication/CANSessionMux.h"
 #include <memory>
 
-#include <sensor_msgs/Joy.h>
 #include "can_talon_srx/CANSend.h"
 #include "can_talon_srx/CANRecv.h"
 #include "can_talon_srx_msgs/control.h"
 #include "can_talon_srx_msgs/status.h"
-//#include "std_msgs/String.h"
 
 CanTalonSRX* motor;
 
@@ -40,123 +38,97 @@ int main(int argc, char **argv) {
 	motor = new CanTalonSRX(1);
 	CanTalonSRX motor2 (2);
 
-	double batteryV;
-	double current;
-	int throttle;
-	int limitswitchfor;
-	int sensorpos;
-	int fsoftenable;
 	double dparam; //variable declaration needs to be outside or else segfault
 	int param;
 	while(ros::ok()) {
-		
 		can_talon_srx_msgs::status status_msg;
-motor->GetFault_OverTemp(param);
-status_msg.Fault_OverTemp = param;
-motor->GetFault_UnderVoltage(param);
-status_msg.Fault_UnderVoltage = param;
-motor->GetFault_ForLim(param);
-status_msg.Fault_ForLim = param;
-motor->GetFault_RevLim(param);
-status_msg.Fault_RevLim = param;
-motor->GetFault_HardwareFailure(param);
-status_msg.Fault_HardwareFailure = param;
-motor->GetFault_ForSoftLim(param);
-status_msg.Fault_ForSoftLim = param;
-motor->GetFault_RevSoftLim(param);
-status_msg.Fault_RevSoftLim = param;
-motor->GetStckyFault_OverTemp(param);
-status_msg.StckyFault_OverTemp = param;
-motor->GetStckyFault_UnderVoltage(param);
-status_msg.StckyFault_UnderVoltage = param;
-motor->GetStckyFault_ForLim(param);
-status_msg.StckyFault_ForLim = param;
-motor->GetStckyFault_RevLim(param);
-status_msg.StckyFault_RevLim = param;
-motor->GetStckyFault_ForSoftLim(param);
-status_msg.StckyFault_ForSoftLim = param;
-motor->GetStckyFault_RevSoftLim(param);
-status_msg.StckyFault_RevSoftLim = param;
-motor->GetAppliedThrottle(param);
-status_msg.AppliedThrottle = param;
-motor->GetCloseLoopErr(param);
-status_msg.CloseLoopErr = param;
-motor->GetFeedbackDeviceSelect(param);
-status_msg.FeedbackDeviceSelect = param;
-motor->GetModeSelect(param);
-status_msg.ModeSelect = param;
-motor->GetLimitSwitchEn(param);
-status_msg.LimitSwitchEn = param;
-motor->GetLimitSwitchClosedFor(param);
-status_msg.LimitSwitchClosedFor = param;
-motor->GetLimitSwitchClosedRev(param);
-status_msg.LimitSwitchClosedRev = param;
-motor->GetSensorPosition(param);
-status_msg.SensorPosition = param;
-motor->GetSensorVelocity(param);
-status_msg.SensorVelocity = param;
-motor->GetCurrent(dparam);
-status_msg.Current = dparam;
-motor->GetBrakeIsEnabled(param);
-status_msg.BrakeIsEnabled = param;
-motor->GetEncPosition(param);
-status_msg.EncPosition = param;
-motor->GetEncVel(param);
-status_msg.EncVel = param;
-motor->GetEncIndexRiseEvents(param);
-status_msg.EncIndexRiseEvents = param;
-motor->GetQuadApin(param);
-status_msg.QuadApin = param;
-motor->GetQuadBpin(param);
-status_msg.QuadBpin = param;
-motor->GetQuadIdxpin(param);
-status_msg.QuadIdxpin = param;
-motor->GetAnalogInWithOv(param);
-status_msg.AnalogInWithOv = param;
-motor->GetAnalogInVel(param);
-status_msg.AnalogInVel = param;
-motor->GetTemp(dparam);
-status_msg.Temp = dparam;
-motor->GetBatteryV(dparam);
-status_msg.BatteryV = dparam;
-motor->GetResetCount(param);
-status_msg.ResetCount = param;
-motor->GetResetFlags(param);
-status_msg.ResetFlags = param;
-motor->GetFirmVers(param);
-status_msg.FirmVers = param;
-motor->GetPulseWidthPosition(param);
-status_msg.PulseWidthPosition = param;
-motor->GetPulseWidthVelocity(param);
-status_msg.PulseWidthVelocity = param;
-motor->GetPulseWidthRiseToFallUs(param);
-status_msg.PulseWidthRiseToFallUs = param;
-motor->GetPulseWidthRiseToRiseUs(param);
-status_msg.PulseWidthRiseToRiseUs = param;
-motor->IsPulseWidthSensorPresent(param);
-status_msg.IsPulseWidthSensorPresent = param;
+
+		motor->GetFault_OverTemp(param);
+		status_msg.Fault_OverTemp = param;
+		motor->GetFault_UnderVoltage(param);
+		status_msg.Fault_UnderVoltage = param;
+		motor->GetFault_ForLim(param);
+		status_msg.Fault_ForLim = param;
+		motor->GetFault_RevLim(param);
+		status_msg.Fault_RevLim = param;
+		motor->GetFault_HardwareFailure(param);
+		status_msg.Fault_HardwareFailure = param;
+		motor->GetFault_ForSoftLim(param);
+		status_msg.Fault_ForSoftLim = param;
+		motor->GetFault_RevSoftLim(param);
+		status_msg.Fault_RevSoftLim = param;
+		motor->GetStckyFault_OverTemp(param);
+		status_msg.StckyFault_OverTemp = param;
+		motor->GetStckyFault_UnderVoltage(param);
+		status_msg.StckyFault_UnderVoltage = param;
+		motor->GetStckyFault_ForLim(param);
+		status_msg.StckyFault_ForLim = param;
+		motor->GetStckyFault_RevLim(param);
+		status_msg.StckyFault_RevLim = param;
+		motor->GetStckyFault_ForSoftLim(param);
+		status_msg.StckyFault_ForSoftLim = param;
+		motor->GetStckyFault_RevSoftLim(param);
+		status_msg.StckyFault_RevSoftLim = param;
+		motor->GetAppliedThrottle(param);
+		status_msg.AppliedThrottle = param;
+		motor->GetCloseLoopErr(param);
+		status_msg.CloseLoopErr = param;
+		motor->GetFeedbackDeviceSelect(param);
+		status_msg.FeedbackDeviceSelect = param;
+		motor->GetModeSelect(param);
+		status_msg.ModeSelect = param;
+		motor->GetLimitSwitchEn(param);
+		status_msg.LimitSwitchEn = param;
+		motor->GetLimitSwitchClosedFor(param);
+		status_msg.LimitSwitchClosedFor = param;
+		motor->GetLimitSwitchClosedRev(param);
+		status_msg.LimitSwitchClosedRev = param;
+		motor->GetSensorPosition(param);
+		status_msg.SensorPosition = param;
+		motor->GetSensorVelocity(param);
+		status_msg.SensorVelocity = param;
+		motor->GetCurrent(dparam);
+		status_msg.Current = dparam;
+		motor->GetBrakeIsEnabled(param);
+		status_msg.BrakeIsEnabled = param;
+		motor->GetEncPosition(param);
+		status_msg.EncPosition = param;
+		motor->GetEncVel(param);
+		status_msg.EncVel = param;
+		motor->GetEncIndexRiseEvents(param);
+		status_msg.EncIndexRiseEvents = param;
+		motor->GetQuadApin(param);
+		status_msg.QuadApin = param;
+		motor->GetQuadBpin(param);
+		status_msg.QuadBpin = param;
+		motor->GetQuadIdxpin(param);
+		status_msg.QuadIdxpin = param;
+		motor->GetAnalogInWithOv(param);
+		status_msg.AnalogInWithOv = param;
+		motor->GetAnalogInVel(param);
+		status_msg.AnalogInVel = param;
+		motor->GetTemp(dparam);
+		status_msg.Temp = dparam;
+		motor->GetBatteryV(dparam);
+		status_msg.BatteryV = dparam;
+		motor->GetResetCount(param);
+		status_msg.ResetCount = param;
+		motor->GetResetFlags(param);
+		status_msg.ResetFlags = param;
+		motor->GetFirmVers(param);
+		status_msg.FirmVers = param;
+		motor->GetPulseWidthPosition(param);
+		status_msg.PulseWidthPosition = param;
+		motor->GetPulseWidthVelocity(param);
+		status_msg.PulseWidthVelocity = param;
+		motor->GetPulseWidthRiseToFallUs(param);
+		status_msg.PulseWidthRiseToFallUs = param;
+		motor->GetPulseWidthRiseToRiseUs(param);
+		status_msg.PulseWidthRiseToRiseUs = param;
+		motor->IsPulseWidthSensorPresent(param);
+		status_msg.IsPulseWidthSensorPresent = param;
 
 		status_pub.publish(status_msg);
-		//motor.Set(.3);
-		//motor2.Set(.3);
-		motor->GetBatteryV(batteryV);
-		motor->GetAppliedThrottle(throttle);
-		motor->GetLimitSwitchClosedFor(limitswitchfor);
-		motor->GetAnalogInWithOv(sensorpos);
-		//motor.SetForwardSoftEnable(1);
-		//motor.GetForwardSoftEnable(fsoftenable);
-
-		std::cout << "BatteryV\t" << batteryV << std::endl;
-		//std::cout << "current\t" << current << std::endl;
-		std::cout << "throttle\t" << throttle << std::endl;
-		std::cout << "limitswitchfor\t" << limitswitchfor << std::endl;
-		std::cout << "sensorpos\t" << sensorpos << std::endl;
-		//std::cout << "fsoftenable\t" << fsoftenable << std::endl;
-		//motor.Set(.1);
-		//ros::Duration(1).sleep();
-		//motor.Set(.3);
-		//ros::Duration(1).sleep();
 		ros::spinOnce();
-		//loop_rate.sleep();
 	}
 }
