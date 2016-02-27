19096f72e9ff4dcfae5ba6488bf5c52cf7094b38
h <dangerousElectrician@gmail.com>
2016-02-27 15:36:23 -0600

    use srv instead of msg

--- src/can_talon_srx_test/scripts/can_talon_srx_test.py
+++ src/can_talon_srx_test/scripts/can_talon_srx_test.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 import rospy
 from can_talon_srx_msgs.msg import control
+from can_talon_srx.srv import *
 from sensor_msgs.msg import Joy
 
 def joyCallback(joy):
 	print(joy.axes[1])
-	msg = control()
-	msg.set = joy.axes[1]
-	pub.publish(msg)
+	rospy.wait_for_service('set')
+	try:
+		set = rospy.ServiceProxy('set', Set)
+		resp = set(joy.axes[1])
+	except rospy.ServiceException,e :
+		print "Service call failed: %s"%e
+	#msg = control()
+	#msg.set = joy.axes[1]
+	#pub.publish(msg)
 
 
 def can_talon_srx_test():
