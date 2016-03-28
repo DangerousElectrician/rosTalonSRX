a58c7e9e2322d14d9237b5afb771f9279544f8be
h <dangerousElectrician@gmail.com>
2016-03-27 21:24:14 -0500

    avoid locking up entirely when sending messages

--- src/ardu_can_bridge/src/ardu_can_bridge_node.cpp
+++ src/ardu_can_bridge/src/ardu_can_bridge_node.cpp
@@ -414,7 +414,7 @@ int main(int argc, char **argv) {
 			repeatread--;
 		} else {
 			std::cout << "timeout " << unsigned(repeatread) << std::endl;
-			ros::getGlobalCallbackQueue()->callAvailable();
+			ros::getGlobalCallbackQueue()->callOne();
 		}
 		//ros::spinOnce();
 	}
