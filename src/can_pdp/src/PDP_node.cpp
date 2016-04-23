f404f72f463d057818f457aaf97df3b76804597c
h <dangerousElectrician@gmail.com>
2016-04-23 01:17:00 -0500

    pdp stuff

---
+++ src/can_pdp/src/PDP_node.cpp
@@ -0,0 +1,19 @@
+#include "HAL/PDP.h"
+#include <iostream>
+#include "ros/ros.h"
+#include <vector>
+#include "FRC_NetworkCommunication/CANSessionMux.h"
+#include <memory>
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "pdp");
+	ros::NodeHandle n;
+
+	init_CANSend(n);
+
+	pdp = new PDP(0);
+
+	while(ros::ok()) {
+
+	}
+}
