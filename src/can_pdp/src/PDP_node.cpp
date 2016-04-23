8872ab33f9df68a246a23b1323180295fe8d8cf4
h <dangerousElectrician@gmail.com>
2016-04-23 12:43:22 -0500

    node for pdp

--- src/can_pdp/src/PDP_node.cpp
+++ src/can_pdp/src/PDP_node.cpp
@@ -4,12 +4,26 @@
 #include <vector>
 #include "FRC_NetworkCommunication/CANSessionMux.h"
 #include <memory>
+#include "can_pdp_msgs/PDP.h"
+#include "std_msgs/Empty.h"
 
 PDP* pdp;
+
+void clearStickyFaultsCallback(const std_msgs::Empty::ConstPtr& msg) {
+
+}
+
+void resetEnergyCallback(const std_msgs::Empty::ConstPtr& msg) {
+
+}
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "pdp");
 	ros::NodeHandle n;
+	
+	ros::Publisher pdp_pub = n.advertise<can_pdp_msgs::PDP>("pdp", 10);
+	ros::Subscriber clearStickyFaults_sub = n.subscribe("clearStickyFaults", 1, clearStickyFaultsCallback);
+	ros::Subscriber resetEnergy_sub = n.subscribe("resetEnergy", 1, resetEnergyCallback);
 
 	init_CANSend(n);
 
@@ -19,6 +33,17 @@ int main(int argc, char **argv) {
 	pdp->GetChannelCurrent(0, current);
 
 	while(ros::ok()) {
+		can_pdp_msgs::PDP pdp_msg;
 
+		for(int i = 0; i < 16; i++) {
+			pdp->GetChannelCurrent(i, pdp_msg.Current[i]);
+		}
+		pdp->GetVoltage(pdp_msg.Voltage);
+		pdp->GetTemperature(pdp_msg.Temperature);
+		pdp->GetTotalCurrent(pdp_msg.TotalCurrent);
+		pdp->GetTotalPower(pdp_msg.TotalPower);
+		pdp->GetTotalEnergy(pdp_msg.TotalEnergy);
+
+		pdp_pub.publish(pdp_msg);
 	}
 }
